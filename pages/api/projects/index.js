8d35e6b9421f3c07ea89c848dfe117615006b4d0
william <williamlibero@profkenny.com.br>
2024-04-16 19:43:01 -0300

    Added return in projects GET route.

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -7,6 +7,6 @@ export default async function handler(req, res) {
   );
   const { data, error } = await supabase.from("projects").select();
 
-  res.status(200).json({ projects: data });
+  // res.status(200).json({ projects: data });
   return { projects: data };
 }
