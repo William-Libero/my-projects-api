93f3566f5c6437f62df0a2ecea2e7152006a047e
william <williamlibero@profkenny.com.br>
2024-04-16 22:14:34 -0300

    Added order by parameter in the api route to get the projects based on id order.

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -5,7 +5,10 @@ export default async function handler(req, res) {
     "https://uawklpreginjapghlqym.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhd2tscHJlZ2luamFwZ2hscXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxMDYwNTIsImV4cCI6MjAyODY4MjA1Mn0.7UD5CBDzyg9si1m5T-v0IRBsuOxPrFpahnVHxVL4RY4"
   );
-  const { data, error } = await supabase.from("projects").select();
+  const { data, error } = await supabase
+    .from("projects")
+    .select()
+    .order("id", { ascending: false });
 
   return res.status(200).json({ projects: data });
 }
